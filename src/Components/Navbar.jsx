baba69ec08ec93bf19ec3309d24c260811d83811
Sandeep Sokle <sandeepsokle12@gmail.com>
2021-10-12 17:14:13 +0530

    up

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,11 +2,16 @@ import "../SCSS/Navbar.scss";
 import "material-icons/iconfont/material-icons.css";
 import { useEffect } from "react";
 import { useState } from "react";
-import imageIcon from "../Img/boongg-white-logo.jpg"
+import imageIcon from "../Img/boongg-white-logo.jpg";
+import imageIconMobile from "../Img/mobileLogo.png";
 
 let Navbar = () => {
   var [location, setLocation] = useState("pune");
   var [scrollVal, setScrollVal] = useState(0);
+  var [windowWidth, setWindowWidth] = useState();
+  var [showLogin, setShowLogin] = useState(false);
+  var [showSearch, setShowSearch] = useState(false);
+  var [showMenuItems, setShowMenuItems] = useState(false);
 
   var onSuccess = (position) => {
     var lat = position.coords.latitude;
@@ -15,9 +20,14 @@ let Navbar = () => {
   };
 
   useEffect(() => {
+    console.log(window.innerWidth);
+    setWindowWidth(window.innerWidth);
+
     window.addEventListener("scroll", (e) => {
       setScrollVal(window.scrollY);
+      setShowMenuItems(false);
     });
+
     return () => {
       window.removeEventListener("scroll", () => {
         console.log("remove Event");
@@ -36,57 +46,94 @@ let Navbar = () => {
       }`}
     >
       <div class="container-fluid inner_nav d-flex justify-content-between row">
-        {console.log(scrollVal)}
-        <div className="col-3 iconContainer">
+        {windowWidth < 1024 ? <div className=" col-sm-1"></div> : ""}
 
-       <img src={imageIcon} alt="" srcset="" />
+        <div className="col-3 ms-5 ms-sm-0 iconContainer">
+          {windowWidth < 470 ? (
+            <img src={imageIconMobile} alt="" srcset="" className="icon" />
+          ) : (
+            <img src={imageIcon} alt="" srcset="" className="icon" />
+          )}
+
         </div>
 
-        <div
-          className={`col-7 navbarInputContainer row navbarLocation  ${
-            scrollVal < 400
-              ? "opacity-0"
-              : scrollVal < 500
-              ? "opacity-50"
-              : "opacity-100"
-          }`}
-        >
-          <div className="col-4 row">
-            <input
-              type="text"
-              name="Location"
-              class="col-10 navbarLocation"
-              placeholder="Location....."
-            />
+        {windowWidth < 1024 ? (
+
+          
+          <div className="col-1 col-sm-5"></div>
+
+        ) : (
+          <div
+            className={`col-7 navbarInputContainer row navbarLocation  ${
+              scrollVal < 400
+                ? "opacity-0"
+                : scrollVal < 500
+                ? "opacity-50"
+                : "opacity-100"
+            }`}
+          >
+            <div className="col-4 row">
+              <input
+                type="text"
+                name="Location"
+                class="col-10 navbarLocation"
+                placeholder="Location....."
+              />
+              <div className="col-2">
+                <span class="material-icons">my_location</span>
+              </div>
+            </div>
+            <div className="col-3">
+              <input
+                type="datetime-local"
+                name="Starting Date"
+                class="navbarStarting_date"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="datetime-local"
+                name="Starting Date"
+                class="navbarEndingDate"
+              />
+            </div>
             <div className="col-2">
-              <span class="material-icons">my_location</span>
+              <button type="button" className="navbarRentBtn">
+                Rent Now
+              </button>
             </div>
           </div>
-          <div className="col-3">
-            <input
-              type="datetime-local"
-              name="Starting Date"
-              class="navbarStarting_date"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="datetime-local"
-              name="Starting Date"
-              class="navbarEndingDate"
-            />
-          </div>
-          <div className="col-2">
-            <button type="button" className="navbarRentBtn">
-              Rent Now
-            </button>
-          </div>
-        </div>
+        )}
 
         <div className="col-2 navbarLoginBtn">
-          <button type="button" class="btn btn-success loginBtn1 opacity-100">
-            Login
-          </button>
+          {windowWidth <= 425 ||
+          (windowWidth > 425 && windowWidth < 1024 && scrollVal >= 500) ? (
+            <span
+              class="material-icons btn navbarMenuBtn col-4"
+              onClick={(e) => {
+                if (showMenuItems) {
+                  setShowMenuItems(false);
+                  e.target.parentNode.removeChild(
+                    e.target.parentNode.querySelector(".menuListItem")
+                  );
+                  return;
+                }
+                setShowMenuItems(true);
+                let menuList = document.createElement("div");
+                menuList.classList.add("menuListItem");
+                menuList.innerHTML = `<div className="menuLoginBtn">Login</div>
+              <div className="menuSearchBtn">Search</div>`;
+                console.log(e.target.parentNode);
+                e.target.parentNode.appendChild(menuList);
+              }}
+            >
+              menu
+            </span>
+          ) : (
+            <button type="button" class="btn btn-success loginBtn1 opacity-100">
+              Login
+            </button>
+          )}
         </div>
         <div className="col-1"></div>
       </div>
