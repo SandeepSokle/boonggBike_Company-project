9ca79e170f7f240e53d9b56730c7d4dd4b9598fa
Sandeep Sokle <sandeepsokle12@gmail.com>
2021-10-12 12:27:41 +0530

    up

--- src/App.js
+++ src/App.js
@@ -6,20 +6,10 @@ import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import "./CSS/App.css"
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
-import { useEffect } from "react";
 
 function App() {
 
-useEffect(()=>{
-  window.addEventListener("scroll",(e)=>{
-    console.log(window.scrollY);
-  })
-  return ()=>{
-    window.removeEventListener("scroll",()=>{
-      console.log("remove Event");
-    });
-  }
-},[])
+
 
   return (
     <div className="App_Container">
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,12 @@
 import "../SCSS/Navbar.scss";
 import "material-icons/iconfont/material-icons.css";
+import { useEffect } from "react";
 import { useState } from "react";
+import imageIcon from "../Img/boongg-white-logo.jpg"
+
 let Navbar = () => {
   var [location, setLocation] = useState("pune");
+  var [scrollVal, setScrollVal] = useState(0);
 
   var onSuccess = (position) => {
     var lat = position.coords.latitude;
@@ -10,57 +14,81 @@ let Navbar = () => {
     setLocation("" + lat + lng);
   };
 
+  useEffect(() => {
+    window.addEventListener("scroll", (e) => {
+      setScrollVal(window.scrollY);
+    });
+    return () => {
+      window.removeEventListener("scroll", () => {
+        console.log("remove Event");
+      });
+    };
+  }, []);
+
   return (
-    <nav class="navbarContainer fixed-top">
+    <nav
+      class={`navbarContainer fixed-top ${
+        scrollVal == 0
+          ? "opacity-0"
+          : scrollVal < 120
+          ? "opacity-50"
+          : "opacity-100"
+      }`}
+    >
       <div class="container-fluid inner_nav d-flex justify-content-between row">
-        <a class="navbar-brand icon col-3" href="#">
-          {" "}
-        </a>
-        
-        {/* <div class="collapse navbar-collapse col-8" id="navbarScroll">
-          <li class="row">
-            <div class="input-group navInput col-md-2">
-              <input
-                type="text"
-                class="form-control"
-                id="inputGroupFile01"
-                placeholder="Location..."
-                value={location}
-              />
-              <label
-                class="input-group-text"
-                for="inputGroupFile01"
-                onClick={() => {
-                  if (navigator.geolocation) {
-                    navigator.geolocation.getCurrentPosition(onSuccess);
-                  } else {
-                    alert("Geolocation is not supported by this browser.");
-                  }
-                }}
-              >
-                Location
-              </label>
-              <input
-                type="datetime-local"
-                class="form-control"
-                id="inputGroupFile01"
-                placeholder="starting Date"
-              />
-              <input
-                type="datetime-local"
-                class="form-control"
-                id="inputGroupFile01"
-                placeholder="ending Date"
-              />
-              <button type="button" class="btn btn-secondary">
-                Rent Now
-              </button>
+        {console.log(scrollVal)}
+        <div className="col-3 iconContainer">
+
+       <img src={imageIcon} alt="" srcset="" />
+        </div>
+
+        <div
+          className={`col-7 navbarInputContainer row navbarLocation  ${
+            scrollVal < 400
+              ? "opacity-0"
+              : scrollVal < 500
+              ? "opacity-50"
+              : "opacity-100"
+          }`}
+        >
+          <div className="col-4 row">
+            <input
+              type="text"
+              name="Location"
+              class="col-10 navbarLocation"
+              placeholder="Location....."
+            />
+            <div className="col-2">
+              <span class="material-icons">my_location</span>
             </div>
-          </li>
-        </div> */}
-        <button type="button" class="btn btn-success loginBtn1 col-1">
-          Login
-        </button>
+          </div>
+          <div className="col-3">
+            <input
+              type="datetime-local"
+              name="Starting Date"
+              class="navbarStarting_date"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="datetime-local"
+              name="Starting Date"
+              class="navbarEndingDate"
+            />
+          </div>
+          <div className="col-2">
+            <button type="button" className="navbarRentBtn">
+              Rent Now
+            </button>
+          </div>
+        </div>
+
+        <div className="col-2 navbarLoginBtn">
+          <button type="button" class="btn btn-success loginBtn1 opacity-100">
+            Login
+          </button>
+        </div>
+        <div className="col-1"></div>
       </div>
     </nav>
   );
